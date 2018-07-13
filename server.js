5039ffba11532fdd52093924d5bb9cf0cd24ea24
unknown <kumaranindo992@gmail.com>
2018-07-13 17:36:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ejs = require('ejs');
+
+var app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    // res.send('<h1>hello express</h1>');
+    res.render('home.ejs',{
+        siteTitle: 'Home',
+        pageTitle: 'Home Page',
+        bodyContain: 'hello home',
+        currentYear: new Date().getFullYear()
+});
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.ejs',{
+        siteTitle: 'About',
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+errorMsg:'this is an error !'
+    });
+})
+
+app.listen(3000,()=>{
+    console.log('server is up on 3000...');
+});
